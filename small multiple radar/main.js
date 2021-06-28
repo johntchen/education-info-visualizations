12266fba349d8e187652bafe9fdf0ff18570b873
John Chen <john.chen@compass360tech.com>
2021-06-28 15:41:34 -0500

    made small multiples with dummy data

--- small multiple radar/main.js
+++ small multiple radar/main.js
@@ -1,20 +1,31 @@
 // set the dimensions and margins of the graph
 var margin = {top: 30, right: 0, bottom: 30, left: 50},
-    width = 210 - margin.left - margin.right,
-    height = 210 - margin.top - margin.bottom;
+    width = 200 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
 
-//Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function(data) {
+let data = [];
+let features = ["% HS","% College", "% Higher Ed"];
+//generate the data
+for (var i = 0; i < 1; i++){
+    var point = {}
+    //each feature will be a random number from 1-9
+    features.forEach(f => point[f] = 1 + Math.random() * 8);
+    data.push(point);
+}
+console.log(data);
 
-  // group the data: I want to draw one line per group
+//Read the dab
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function(dab) {
+
+  // group the dab: I want to draw one line per group
   var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
     .key(function(d) { return d.name;})
-    .entries(data);
+    .entries(dab);
 
   // What is the list of groups?
   allKeys = sumstat.map(function(d){return d.key})
 
-  // Add an svg element for each group. The will be one beside each other and will go on the next row when no more room available
+  // Add an svg element for each group. They will be side by side and will go next row when no more room available
   var svg = d3.select("#my_dataviz")
     .selectAll("uniqueChart")
     .data(sumstat)
@@ -26,48 +37,84 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-  // Add X axis --> it is a date format
-  var x = d3.scaleLinear()
-    .domain(d3.extent(data, function(d) { return d.year; }))
-    .range([ 0, width ]);
-  svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).ticks(3));
+let radialScale = d3.scaleLinear()
+.domain([0,10])
+.range([0,50]);
+let ticks = [5, 10];
 
-  //Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return +d.n; })])
-    .range([ height, 0 ]);
-  svg.append("g")
-    .call(d3.axisLeft(y).ticks(5));
+ticks.forEach(t =>
+svg.append("circle")
+.attr("cx", 100)
+.attr("cy", 100)
+.attr("fill", "none")
+.attr("stroke", "gray")
+.attr("r", radialScale(t))
+);
 
-  // color palette
-  var color = d3.scaleOrdinal()
-    .domain(allKeys)
-    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+// ticks.forEach(t =>
+// svg.append("text")
+// .attr("x", 305)
+// .attr("y", 300 - radialScale(t))
+// .text(t.toString())
+// );
 
-  // Draw the line
-  svg
-    .append("path")
-      .attr("fill", "none")
-      .attr("stroke", function(d){ return color(d.key) })
-      .attr("stroke-width", 1.9)
-      .attr("d", function(d){
-        return d3.line()
-          .x(function(d) { return x(d.year); })
-          .y(function(d) { return y(+d.n); })
-          (d.values)
-      })
+function angleToCoordinate(angle, value){
+let x = Math.cos(angle) * radialScale(value);
+let y = Math.sin(angle) * radialScale(value);
+return {"x": 100 + x, "y": 100 - y};
+}
 
-  // Add titles
-//   svg
-//     .append("text")
-//     .attr("text-anchor", "start")
-//     .attr("y", -5)
-//     .attr("x", 0)
-//     .text(function(d){ return(d.key)})
-//     .style("fill", function(d){ return color(d.key) })
+for (var i = 0; i < features.length; i++) {
+let ft_name = features[i];
+let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+let line_coordinate = angleToCoordinate(angle, 10);
+let label_coordinate = angleToCoordinate(angle, 10.5);
+
+//draw axis line
+svg.append("line")
+.attr("x1", 100)
+.attr("y1", 100)
+.attr("x2", line_coordinate.x)
+.attr("y2", line_coordinate.y)
+.attr("stroke","black");
+
+//draw axis label
+// svg.append("text")
+// .attr("x", label_coordinate.x)
+// .attr("y", label_coordinate.y)
+// .text(ft_name);
+}
+
+let line = d3.line()
+.x(d => d.x)
+.y(d => d.y);
+let colors = ["darkorange", "gray", "navy"];
+
+function getPathCoordinates(data_point){
+let coordinates = [];
+for (var i = 0; i < features.length; i++){
+    let ft_name = features[i];
+    let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+    coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+}
+return coordinates;
+}
+
+for (var i = 0; i < data.length; i ++){
+let d = data[i];
+let color = colors[i];
+let coordinates = getPathCoordinates(d);
+
+//draw the path element
+svg.append("path")
+.datum(coordinates)
+.attr("d",line)
+.attr("stroke-width", 3)
+.attr("stroke", color)
+.attr("fill", color)
+.attr("stroke-opacity", 1)
+.attr("opacity", 0.5);
+}
 
 
 
