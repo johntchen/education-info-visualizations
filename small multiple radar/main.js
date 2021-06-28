2090dcb92670efbd24d5d27808e92afaa627ec31
John Chen <john.chen@compass360tech.com>
2021-06-28 12:16:52 -0500

    tested out small multiples and radars

--- small multiple radar/main.js
+++ small multiple radar/main.js
@@ -1,250 +1,75 @@
-//instantiate tooltip
-var toolTip = d3.tip()
-    .attr("class", "d3-tip")
-    .offset([-12, 0])
-    .html(function(d) {
-        return "<h5>"+d['State']+"</h5>";
-    });
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 0, bottom: 30, left: 50},
+    width = 210 - margin.left - margin.right,
+    height = 210 - margin.top - margin.bottom;
 
-var svg = d3.select('svg');
-svg.call(toolTip);
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function(data) {
 
-// Get layout parameters
-var svgWidth = +svg.attr('width');
-var svgHeight = +svg.attr('height');
+  // group the data: I want to draw one line per group
+  var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+    .key(function(d) { return d.name;})
+    .entries(data);
 
-var padding = {t: 40, r: 40, b: 40, l: 40};
-var cellPadding = 10;
+  // What is the list of groups?
+  allKeys = sumstat.map(function(d){return d.key})
 
-// Create a group element for appending chart elements
-var chartG = svg.append('g')
-    .attr('transform', 'translate('+[padding.l, padding.t]+')');
+  // Add an svg element for each group. The will be one beside each other and will go on the next row when no more room available
+  var svg = d3.select("#my_dataviz")
+    .selectAll("uniqueChart")
+    .data(sumstat)
+    .enter()
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-var dataAttributes = ['total population', 'revenue', 'federal aid', 'outstanding debt', 'per pupil salaries', 'per pupil employee benefits', 'average sat score', 'average act score', 'act % students tested', 'IEP per 1000', 'youth substance abuse per 100', 'high school diploma %', 'bachelor degree %', 'advanced degree %', 'school lunch students', 'school lunch reduced %'];
-var N = dataAttributes.length;
+  // Add X axis --> it is a date format
+  var x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.year; }))
+    .range([ 0, width ]);
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).ticks(3));
 
-// Compute chart dimensions
-var cellWidth = (svgWidth - padding.l - padding.r) / N;
-var cellHeight = (svgHeight - padding.t - padding.b) / N;
+  //Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return +d.n; })])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y).ticks(5));
 
-// Global x and y scales to be used for all SplomCells
-var xScale = d3.scaleLinear().range([0, cellWidth - cellPadding]);
-var yScale = d3.scaleLinear().range([cellHeight - cellPadding, 0]);
-// axes that are rendered already for you
-var xAxis = d3.axisTop(xScale).ticks(6).tickSize(-cellHeight * N, 0, 0);
-var yAxis = d3.axisLeft(yScale).ticks(6).tickSize(-cellWidth * N, 0, 0);
-// Ordinal color scale for cylinders color mapping
-var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-// Map for referencing min/max per each attribute
-var extentByAttribute = {};
-// Object for keeping state of which cell is currently being brushed
-var brushCell;
+  // color palette
+  var color = d3.scaleOrdinal()
+    .domain(allKeys)
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
 
-// ****** Add reusable components here ****** //
-function SplomCell(x, y, col, row) {
-    this.x = x;
-    this.y = y;
-    this.col = col;
-    this.row = row;
-}
+  // Draw the line
+  svg
+    .append("path")
+      .attr("fill", "none")
+      .attr("stroke", function(d){ return color(d.key) })
+      .attr("stroke-width", 1.9)
+      .attr("d", function(d){
+        return d3.line()
+          .x(function(d) { return x(d.year); })
+          .y(function(d) { return y(+d.n); })
+          (d.values)
+      })
 
-//instantiate brush object
-var brush = d3.brush()
-    .extent([[0, 0], [cellWidth - cellPadding, cellHeight - cellPadding]])
-    .on("start", brushstart)
-    .on("brush", brushmove)
-    .on("end", brushend);
-
-
-d3.csv('education.csv', dataPreprocessor).then(function(dataset) {
-    
-        //cars = dataset;
-
-        // Create map for each attribute's extent
-        dataAttributes.forEach(function(attribute){
-            extentByAttribute[attribute] = d3.extent(dataset, function(d){
-                return d[attribute];
-            });
-        });
-
-        // Pre-render gridlines and labels
-        chartG.selectAll('.x.axis')
-            .data(dataAttributes)
-            .enter()
-            .append('g')
-            .attr('class', 'x axis')
-            .attr('transform', function(d,i) {
-                return 'translate('+[(N - i - 1) * cellWidth + cellPadding / 2, 0]+')';
-            })
-            .each(function(attribute){
-                xScale.domain(extentByAttribute[attribute]);
-                d3.select(this).call(xAxis);
-                d3.select(this).append('text')
-                    .text(attribute)
-                    .attr('class', 'axis-label')
-                    .attr('transform', 'translate('+[cellWidth / 2, -20]+')');
-            });
-        chartG.selectAll('.y.axis')
-            .data(dataAttributes)
-            .enter()
-            .append('g')
-            .attr('class', 'y axis')
-            .attr('transform', function(d,i) {
-                return 'translate('+[0, i * cellHeight + cellPadding / 2]+')';
-            })
-            .each(function(attribute){
-                yScale.domain(extentByAttribute[attribute]);
-                d3.select(this).call(yAxis);
-                d3.select(this).append('text')
-                    .text(attribute)
-                    .attr('class', 'axis-label')
-                    .attr('transform', 'translate('+[-26, cellHeight / 2]+')rotate(270)');
-            });
+  // Add titles
+//   svg
+//     .append("text")
+//     .attr("text-anchor", "start")
+//     .attr("y", -5)
+//     .attr("x", 0)
+//     .text(function(d){ return(d.key)})
+//     .style("fill", function(d){ return color(d.key) })
 
 
-        // ********* Your data dependent code goes here *********//
-        var cells = [];
-        dataAttributes.forEach(function(attrX, col){
-            dataAttributes.forEach(function(attrY, row){
-                cells.push(new SplomCell(attrX, attrY, col, row));
-            });
-        });
-        console.log(cells);
 
-        //initialize
-        SplomCell.prototype.init = function(g) {
-            var cell = d3.select(g);
 
-            cell.append('rect')
-              .attr('class', 'frame')
-              .attr('width', cellWidth - cellPadding)
-              .attr('height', cellHeight - cellPadding);
-        }
-
-        //update cell based on incoming data
-        SplomCell.prototype.update = function(g, data) {
-            var cell = d3.select(g);
-
-            // Update the global x,yScale objects for this cell's x,y attribute domains
-            xScale.domain(extentByAttribute[this.x]);
-            yScale.domain(extentByAttribute[this.y]);
-
-            // Save a reference of this SplomCell, to use within anon function scopes
-            var _this = this;
-
-            var dots = cell.selectAll('.dot')
-                .data(data, function(d){
-                    return d.name +'-'+d.year+'-'+d.cylinders; // Create a unique id for the car
-                });
-
-            var dotsEnter = dots.enter()
-                .append('circle')
-                .attr('class', 'dot')
-                .style("fill", function(d) { return colorScale(d.cylinders); })
-                .attr('r', 4);
-
-            dots.merge(dotsEnter).attr('cx', function(d){
-                    return xScale(d[_this.x]);
-                })
-                .attr('cy', function(d){
-                    return yScale(d[_this.y]);
-                });
-            //tooltip
-            dotsEnter.on('mouseover', toolTip.show)
-                .on('mouseout', toolTip.hide);
-
-            dots.exit().remove();
-        }
-
-        //drawing the cells
-        var cellEnter = chartG.selectAll('.cell')
-            .data(cells)
-            .enter()
-            .append('g')
-            .attr('class', 'cell')
-            .attr("transform", function(d) {
-                // Start from the far right for columns to get a better looking chart
-                var tx = (N - d.col - 1) * cellWidth + cellPadding / 2;
-                var ty = d.row * cellHeight + cellPadding / 2;
-                return "translate("+[tx, ty]+")";
-        });
-
-        cellEnter.append('g')
-            .attr('class', 'brush')
-            .call(brush);
-
-        cellEnter.each(function(cell){
-            cell.init(this);
-            cell.update(this, dataset);
-        });
-        
-    });
-
-// *********Event listener functions go here *********//
-function brushstart(cell) {
-    // cell is the SplomCell object
-
-    // Check if this g element is different than the previous brush
-    if(brushCell !== this) {
-
-        // Clear the old brush
-        brush.move(d3.select(brushCell), null);
-
-        // Update the global scales for the subsequent brushmove events
-        xScale.domain(extentByAttribute[cell.x]);
-        yScale.domain(extentByAttribute[cell.y]);
-
-        // Save the state of this g element as having an active brush
-        brushCell = this;
-    }
-}
-
-function brushmove(cell) {
-    // cell is the SplomCell object
-
-    // Get the extent or bounding box of the brush event, this is a 2x2 array
-    var e = d3.event.selection;
-    if(e) {
-
-        // Select all .dot circles, and add the "hidden" class if the data for that circle
-        // lies outside of the brush-filter applied for this SplomCells x and y attributes
-        svg.selectAll(".dot")
-            .classed("hidden", function(d){
-                return e[0][0] > xScale(d[cell.x]) || xScale(d[cell.x]) > e[1][0]
-                    || e[0][1] > yScale(d[cell.y]) || yScale(d[cell.y]) > e[1][1];
-            })
-    }
-}
-
-function brushend() {
-    // If there is no longer an extent or bounding box then the brush has been removed
-    if(!d3.event.selection) {
-        // Bring back all hidden .dot elements
-        svg.selectAll('.hidden').classed('hidden', false);
-        // Return the state of the active brushCell to be undefined
-        brushCell = undefined;
-    }
-}
-
-// Remember code outside of the data callback function will run before the data loads
-
-function dataPreprocessor(row) {
-    return {
-        'total population': row['total_population'],
-        'revenue': +row['total_revenue'],
-        'federal aid': +row['total_federal_aid'],
-        'outstanding debt': +row['total_outstanding_debt'],
-        'per pupil salaries': +row['per_pupil_salaries'],
-        'per pupil employee benefits': +row['per_pupil_employee_benefits'],
-        'average sat score': +row['sat_average_score'],
-        'average act score': +row['avg_act_composite_score'],
-        'act % students tested': +row['act_percent_students_tested'],
-        'IEP per 1000': +row['IEP_per_1000'],
-        'youth substance abuse per 100': +row['youth_substance_use_per_100'],
-        'high school diploma %': +row['educational_attainment_hs_plus'],
-        'bachelor degree %': +row['educational_attainment_bachelor_plus'],
-        'advanced degree %': +row['educational_attainment_advanced_degree_plus'],
-        'school lunch students': +row['school_lunches_num_students_enrolled'],
-        'school lunch reduced %': +row['school_lunches_percent_students_free_lunch']
-    };
-}
+})
